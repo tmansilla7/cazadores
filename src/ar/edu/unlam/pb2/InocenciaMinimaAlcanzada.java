bb17bedfa0f9a30a10b9aa908ba84539d3efd51a
Tomás Castro Mansilla <tmansilla2001@gmail.com>
2025-12-19 17:12:59 -0300

    Cambios

--- src/ar/edu/unlam/pb2/InocenciaMinimaAlcanzada.java
+++ src/ar/edu/unlam/pb2/InocenciaMinimaAlcanzada.java
@@ -3,7 +3,7 @@ package ar.edu.unlam.pb2;
 public class InocenciaMinimaAlcanzada extends Exception{
 
 	public InocenciaMinimaAlcanzada() {
-		super("");
+		super("La inocencia minima fue alcanzada. ");
 	}
 	
 }
